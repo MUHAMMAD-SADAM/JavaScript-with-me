5ac24fabd4fd19d85789d6f88f2e1decb2969ddd
SadamMuneer <sm78167816@gmail.com>
2024-03-09 23:08:45 +0500

    map

--- 19higher_Loops.js
+++ 19higher_Loops.js
@@ -18,3 +18,33 @@ for (const name of my_Name) {
   }
   console.log(`Each character is : ${name}`);
 }
+
+// Map
+const map = new Map();
+map.set("students01", "Sadam");
+map.set("students02", "Ali");
+map.set("students03", "Ahmad");
+for (const [key, value] of map) {
+  console.log(key, "-:", value);
+}
+
+// can we iterate the object as i iterate the map
+// const myobject = {
+//   students01: "Sadam",
+//   students02: "Ali",
+//   students03: "Ahmad",
+// };
+// for (const [key, value] of myobject) {
+//   console.log(key, "-:", value);
+// }
+//  hense , we cant iterate the object as i iterate the map
+
+const langauges = {
+  js: "javascript",
+  py: "python",
+  ja: "java",
+  cpp: "c++",
+};
+for (const key in langauges) {
+  console.log(`${key} is the shortcut of ${languages[key]}`);
+}
