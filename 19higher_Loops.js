6eba980c2391db8344143731ec05034e5508059b
SadamMuneer <sm78167816@gmail.com>
2024-03-10 23:32:52 +0500

    for_each and for_in loops

--- 19higher_Loops.js
+++ 19higher_Loops.js
@@ -39,12 +39,58 @@ for (const [key, value] of map) {
 // }
 //  hense , we cant iterate the object as i iterate the map
 
-const langauges = {
+const languages = {
   js: "javascript",
   py: "python",
   ja: "java",
   cpp: "c++",
 };
-for (const key in langauges) {
+for (const key in languages) {
   console.log(`${key} is the shortcut of ${languages[key]}`);
 }
+
+const myArrray = ["sadam", "python", "java", 4, 5];
+for (const key in myArrray) {
+  console.log(myArrray[key]);
+}
+
+// Map
+const map2 = new Map();
+map.set("students01", "Sadam");
+map.set("students02", "Ali");
+map.set("students03", "Ahmad");
+for (const key in map2) {
+  console.log(key);
+}
+// we cant iterate the map , because map is un iterateable
+
+// for each Loop
+
+const coding = ["JavaScript", "python", "java"];
+// coding.forEach(function (item) {
+//   console.log(item);
+// });
+
+// coding.forEach((item) => {
+//   console.log(item);
+// });
+
+function pritme(item, index, arr) {
+  console.log(item, index, arr);
+}
+coding.forEach(pritme);
+
+const myCoding = [
+  {
+    languageName: "javaScript",
+  },
+  {
+    languageName: "java",
+  },
+  {
+    languageName: "c++",
+  },
+];
+myCoding.forEach((item) => {
+  console.log(item);
+});
